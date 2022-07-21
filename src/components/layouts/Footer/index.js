f942e7dac629e5922976c4d04cd3bafd06546904
mvn-xinhnguyen2-dn <xinh.nguyen2@monstar-lab.com>
2022-07-21 21:46:22 +0700

    fix footer

--- src/components/layouts/Footer/index.js
+++ src/components/layouts/Footer/index.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Footer() {
   return (
   <footer className="page-footer">
-    <div className="waves">
+    {/* <div className="waves">
       <div className="wave" id="wave1"></div>
       <div className="wave" id="wave2"></div>
       <div className="wave" id="wave3"></div>
       <div className="wave" id="wave4"></div>
-    </div>
+    </div> */}
     <div className="footer-info">2022 - Create by Truc Xinh</div>
   </footer>
   )
